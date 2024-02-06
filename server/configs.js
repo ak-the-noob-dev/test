da14d60caf8db902a77f71abe290bdfbcf35f3cf
ak-the-noob-dev <thebackendtech1011@gmail.com>
2024-02-06 21:40:05 +0530

    __env fix

--- server/configs.js
+++ server/configs.js
@@ -1,4 +1,10 @@
 import dotenv from "dotenv";
-dotenv.config({ path: "./.env" });
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+dotenv.config({ path: join(__dirname, ".env") });
 console.log(process.env);
 export const __env = process.env;
